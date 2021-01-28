916af65658f15266075c0a80e26c404dc78a3607
Tristan Louth-Robins <tristanlouthrobins@MacBook-Pro-4.local>
2021-01-28 22:05:55 +1030

    Streamlined code. Works beautifully!

--- rainfall.R
+++ rainfall.R
@@ -3,15 +3,14 @@ library(bomrang)
 
 ?bomrang # bomrang package help
 
-id <- c("023000")                                 # target weather station(s) for data
-target <- c("2017","2018", "2019","2020", "2021") # target years for filtering
+id <- c("023000") # target weather station(s) for data
+# 23000 = Adelai
 
 # Retrieve historical data from designated weather station
 rain_data <- get_historical(stationid = id,
                             latlon = NULL,
                             radius = NULL,
                             type = "rain") %>% 
-  filter(year %in% target) %>% 
   filter(rainfall > -0.1) %>% 
   select(-1, -7, -8)  # removes 'product_code', 'period' and 'quality' columns
   
@@ -27,61 +26,54 @@ rain_df.1 <- rain_df.1 %>%
         Year = as_factor(Year))
   
 levels(rain_df.1$Month) <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec") 
-  
-# Data tidy (part 2) note: reassign to 'rain_df.2' for debugging purposes.
-rain_df.2 <- rain_df.1
 
 # calculate cumulative and monthly rainfall per year
 
-cr.18 <- rain_df.2 %>% filter(Year == 2018) %>% mutate(CR = cumsum(rainfall))
-avg_m.18 <- rain_df.2 %>% filter(Year == 2018) %>% 
-  group_by(Month) %>% summarise(val = mean(rainfall)) %>% mutate(Year = paste0("2018"))
+rain_df.2 <- rain_df.1 %>% 
+  group_by(Year) %>% 
+  mutate(cr = cumsum(rainfall))
 
-cr.19 <- rain_df.2 %>% filter(Year == 2019) %>% mutate(CR = cumsum(rainfall))
-avg_m.19 <- rain_df.2 %>% filter(Year == 2019) %>% 
-  group_by(Month) %>% summarise(val = mean(rainfall)) %>% mutate(Year = paste0("2019"))
+rain_mean.month <- rain_df.2 %>% 
+  group_by(Year, Month) %>% 
+  summarise(mmr = mean(rainfall))
 
-cr.20 <- rain_df.2 %>% filter(Year == 2020) %>% mutate(CR = cumsum(rainfall))
-avg_m.20 <- rain_df.2 %>% filter(Year == 2020) %>% 
-  group_by(Month) %>% summarise(val = mean(rainfall)) %>% mutate(Year = paste0("2020"))
-
-cr.21 <- rain_df.2 %>% filter(Year == 2021) %>% mutate(CR = cumsum(rainfall))
-avg_m.21 <- rain_df.2 %>% filter(Year == 2021) %>% 
-  group_by(Month) %>% summarise(val = mean(rainfall)) %>% mutate(Year = paste0("2021"))
-
-# Binding the relevant dataframes together
-
-rain_df.2 <- do.call("rbind", list(cr.18, cr.19, cr.20, cr.21))
-rain_df.mean_m <- do.call("rbind", list(avg_m.18, avg_m.19, avg_m.20, avg_m.21)) 
-
-# Setting up yearly summary averages 
-
-rain_df.mean_yr <- rain_df.2 %>% group_by(Year) %>% 
-  summarise(mean_rainfall = mean(rainfall))
-
-#
+rain_mean.year <- rain_df.2 %>% 
+  group_by(Year) %>% 
+  summarise(ymr = mean(rainfall))
 
 # Plotting the data: 0) Defining a custom theme for the visualisations
 
 theme_bom <- function(){
-  theme(plot.background = element_rect(fill = "green"),
-        panel.background = element_rect(fill = "lightgreen"))
+  theme(
+    text = element_text(family = "Arial"),
+    plot.margin = margin(5,2,5,2, "mm"),
+    plot.background = element_rect(fill = "mistyrose1"),
+    panel.background = element_rect(fill = "lightyellow2"),
+    legend.title = element_text(family = "Courier", size = 9),
+    legend.text = element_text(family = "Courier", size = 8),
+    legend.background = element_rect(fill = "mistyrose2")
+  )
 }
 
 # Plotting the data: 1) Plot of daily and cumulative rainfall
 
-plot1 <- ggplot(data = rain_df.2[rain_df.2$Year != "2017",],
-                aes(x = Date, y = rainfall, colour = Year))
+year_range <- c("2018", "2019", "2020")
+max.yr <- rain_df.2 %>% filter(Year %in% year_range) %>% group_by(Year) %>% summarise(max = max(cr))
 
-plot1 +
-  geom_line(aes(x = Date, y = CR), colour = "sienna2") +
+plot1 <- ggplot(data = rain_df.2[rain_df.2$Year %in% yearly_range,],
+                aes(x = Date, y = rainfall, colour = Year)) +
+  geom_line(aes(x = Date, y = cr), colour = "sienna2") +
   geom_point(aes(y = rainfall), alpha = 0.3) +
   theme_classic() +
+  theme_bom() +
   labs(
     title = "Daily & cumulative rainfall 2018 - 2021",
     x = "",
     y = "rainfall in mm",
-    caption = "Source: Bureau of Meterology, Adelaide City weather station. Data pulled using R package, 'bomrang'."
+    caption = "Source: Bureau of Meterology."
   )
 
-plot1
+plot1 +
+  geom_hline(data = max.yr, aes(yintercept = max, colour = Year), linetype = "dotdash")  
+  
+# add labels to these intercepts
